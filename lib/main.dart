dcb9a4210b1969408c2e797a659cfee78744a12e
mosta <mostafakhaled835@outlook.com>
2022-07-07 23:59:13 +0300

    Code Formatting

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 import 'views/home_page.dart';
 import 'Localization/app_localizations_setup.dart';
@@ -22,6 +23,8 @@ void main() async{
 
   // Pass all uncaught errors from the framework to Firebase Crashlytics.
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  FlutterNativeSplash.remove();
 
   runApp(const MyApp());
 }
