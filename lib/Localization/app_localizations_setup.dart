4967ff2e60ad54a0ca275e1aee9fab434fa2c7c5
RoukayaZaki <roka.zaki2002@gmail.com>
2022-07-09 03:01:03 +0200

    Localization

--- lib/Localization/app_localizations_setup.dart
+++ lib/Localization/app_localizations_setup.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 Locale? localeResolutionCallback(
     Locale? locale, Iterable<Locale>? supportedLocales) {
   for (final Locale supportedLocale in supportedLocales!) {
-    if (supportedLocale.languageCode == locale!.languageCode &&
-        supportedLocale.countryCode == locale.countryCode) {
+    if (supportedLocale.languageCode == locale!.languageCode) {
       return supportedLocale;
     }
   }
